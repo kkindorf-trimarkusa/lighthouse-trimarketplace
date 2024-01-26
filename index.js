a928cb45ab849dcc3cc962a3588fb8f1cdbadac1
Kindorf <kevin.kindorf@trimarkusa.com>
2024-01-26 12:29:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+import lighthouse from 'lighthouse';
+import { launch } from 'chrome-launcher';
+import url from 'url';
+import fs from 'fs';
+import puppeteer from 'puppeteer';
+
+let siteUrl = 'https://qa.trimarketplace.com';
+
+async function asyncCall() {
+    const urlObj = new URL(siteUrl);
+    let dirName = urlObj.host;
+    if (urlObj.pathname !== "/") {
+        dirName = dirName + urlObj.pathname.replace(/\//g, "_");
+    }
+    if (!fs.existsSync(dirName)) {
+        fs.mkdirSync(dirName);
+    }
+    let lhOpts = {
+        output: 'html',
+        disableStorageReset: true
+    }
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(siteUrl, {
+        waitUntil: ['load']
+    })
+
+
+    // run an initial lighthouse report for the login page before filling in form for authentication
+    let initialPageresult = await lighthouse(siteUrl, lhOpts, undefined, page);
+    const lhr = initialPageresult.report;
+    fs.writeFile(`login.html`, lhr, err => {
+        if (err) throw err;
+    });
+    const selector = '.onpagelogin form .login-btn';
+    const btn = await page.$(selector)
+    const present = await btn.isVisible()
+    console.log(present)
+
+
+    //start authenticating to log into the site
+    await page.click('.cc-compliance')
+    await page.type('.onpagelogin form .username-input', '1_pw_approver1@trimarkusa.com')
+    await page.type('.onpagelogin form .password-input', 'Welcome1$')
+    await Promise.all([
+        page.click('.onpagelogin form .login-btn'),
+        page.waitForNavigation()
+    ]);
+    console.log('finished login');
+
+    let urlstoTest = [
+        { pageName: 'my-account', url: "https://qa.trimarketplace.com/my-account" },
+        { pageName: 'my-catalog', url: "https://qa.trimarketplace.com/mycatalog" }
+    ]
+
+
+
+    for (let i = 0; i < urlstoTest.length; i++) {
+        console.log(urlstoTest[i])
+        await page.goto(urlstoTest[i].url)
+        const result = await lighthouse(urlstoTest[i].url, lhOpts, undefined, page);
+        const lhr = result.report;
+        fs.writeFile(`${urlstoTest[i].pageName}.html`, lhr, err => {
+            if (err) throw err;
+        });
+    }
+
+
+
+    // // access history and evaluate last url of page
+    // const session = await page.target().createCDPSession();
+    // const history = await session.send("Page.getNavigationHistory");
+    // const last = history.entries[history.entries.length - 2];
+
+    // // create a new page and go back
+    // // important: the page created here does not share the history
+    // const backPage = await browser.newPage();
+    // await backPage.goto(last.url);
+
+    // // see results
+    // await page.screenshot({ path: "page.png" });
+    // await backPage.screenshot({ path: "back-page.png" });
+
+    // uncomment if you use headless chrome
+    // await browser.close();
+}
+
+
+asyncCall();
+
+
+
+
+
+
+
+
+// const browser = await puppeteer.launch({
+//     // Set to false if you want to see the script in action.
+//     headless: 'new',
+//     slowMo: 50,
+// });
+// const page = await browser.newPage();
+// console.log(browser)
+
+// await page.goto(siteUrl);
+
+// console.log('Starting login, entering username and password...');
+// const emailInput = await page.$('input.username-input');
+// await emailInput.type('1_pw_approver1@trimarkusa.com');
+// const passwordInput = await page.$('input.password-input');
+// await passwordInput.type('Welcome1$');
+// await Promise.all([
+//     page.$eval('.login-form', form => form.submit()),
+//     page.waitForNavigation(),
+// ]);
+
+// console.log('finished login');
+// console.log(page.url())
+
+// await Promise.all([
+//     page.goto("https://qa.trimarketplace.com/my-account"),
+//     page.waitForNavigation()
+// ]);
+
+
