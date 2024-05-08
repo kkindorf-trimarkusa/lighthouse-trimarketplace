1aac9fd50ac5e3817a65e0d3cd4f4b7355ad078e
Kindorf <kevin.kindorf@trimarkusa.com>
2024-05-08 12:05:21 -0400

    updates

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ let siteUrl = 'https://shop.trimarketplace.com/login';
 import config from './config/desktop-config.js';
 import buildHtml from './build-html.js';
 import buildNewDate from './build-new-date.js';
-let dirName = "https://shop.trimarketplace.com" + `-${buildNewDate()}`
 
 async function asyncCall() {
+    const urlObj = new URL(siteUrl);
+    let dirName = urlObj.host;
+    if (urlObj.pathname !== "/") {
+        dirName = dirName + urlObj.pathname.replace(/\//g, "_");
+    }
     if (!fs.existsSync(dirName)) {
         fs.mkdirSync(dirName);
     }
